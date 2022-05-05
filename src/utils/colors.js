e520af9a49dc2dbc358fc83ef8db7f8da1550a7e
Hein Htut Aung <heinhtutaung@zig.live>
2022-05-05 22:46:19 +0630

    Big Bang

---
+++ src/utils/colors.js
@@ -0,0 +1,38 @@
+export const white = "#FFFFFF";
+
+export const red = "#A8000B";
+
+export const orange = "#EF631A";
+
+export const yellow = "#FFC107";
+
+export const green = "#00847F";
+
+export const blue = {
+  100: "#080C4E",
+  80: "#0142AF",
+  60: "#044CC5",
+  40: "#008BF8",
+  20: "#7BD7FF",
+  10: "#D2E2F1",
+  
+  link: "#0275D8",
+  secondaryButton: "#DEE8F9",
+};
+
+export const grey = {
+  100: "#333333",
+  80: "#3E3E3E",
+  60: "#757575",
+  40: "#D7D8D6",
+  20: "#E6E6E6",
+  10: "#F7F8F9",
+};
+
+export const tint = {
+  100: "rgba(51, 51, 51, 0.85)",
+  80: "rgba(51, 51, 51, 0.45)",
+  60: "rgba(51, 51, 51, 0.22)",
+  40: "rgba(8, 12, 78, 0.18)",
+  10: "rgba(8, 12, 78, 0.05)",
+}
---
+++ src/utils/typography.js
@@ -0,0 +1,157 @@
+export const primaryFont = "Barlow";
+
+export const headerScales = [
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "28px",
+    lineHeight: "34px",
+  },
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: "28px",
+    lineHeight: "34px",
+  },
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "22px",
+    lineHeight: "28px",
+  },
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "18px",
+    lineHeight: "18px",
+  },
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "16px",
+    lineHeight: "21px",
+  },
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "14px",
+    lineHeight: "17px",
+  },
+];
+
+export const paragraphScales = [
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: "16px",
+    lineHeight: "21px",
+  },
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 600,
+    fontSize: "16px",
+    lineHeight: "21px",
+  },
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: "14px",
+    lineHeight: "18px",
+  },
+];
+
+export const labelScales = [
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: "14px",
+    lineHeight: "21px",
+  },
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: "13px",
+    lineHeight: "18px",
+  },
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: "12px",
+    lineHeight: "14px",
+  },
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: "11px",
+    lineHeight: "13px",
+  },
+];
+
+export const linkScales = [
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "16px",
+    lineHeight: "24px",
+  },
+  {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "12px",
+    lineHeight: "18px",
+  },
+];
+
+export const footNoteScales = {
+  fontFamily: primaryFont,
+  fontStyle: "normal",
+  fontWeight: 500,
+  fontSize: "12px",
+  lineHeight: "18px",
+};
+
+export const buttonScales = {
+  primary: {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "16px",
+    lineHeight: "24px",
+  },
+  primaryPressed: {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "16px",
+    lineHeight: "24px",
+  },
+  secondary: {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "16px",
+    lineHeight: "24px",
+  },
+  disabled: {
+    fontFamily: primaryFont,
+    fontStyle: "normal",
+    fontWeight: 700,
+    fontSize: "16px",
+    lineHeight: "24px",
+  },
+};
