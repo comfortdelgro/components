878e12fc8d3b9c3959307e79edde5d1427a24710
Hein Htut Aung <heinhtutaung@zig.live>
2022-06-07 08:05:43 +0630

    feat: configurable theme system
    	- theme is now configurable from consumer
    	- fully merge theme with base web theme

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -1,26 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyle, defaultTheme, } from './utils';
+import { GlobalStyle, defaultTheme } from './utils';
 
 import {Client as Styletron} from 'styletron-engine-atomic';
 import {Provider as StyletronProvider} from 'styletron-react';
 import {LightTheme, BaseProvider, createTheme} from 'baseui';
 
-const engine = new Styletron();
-const mergedTheme = createTheme({
-  ...LightTheme,
-  primaryFontFamily: defaultTheme.primaryFontFamily,
-});
-console.debug("Theme", LightTheme);
-const Theme = ({ children }) => (
-  <ThemeProvider theme={defaultTheme}>
-    <StyletronProvider value={engine}>
-      <BaseProvider theme={mergedTheme}>
-        <GlobalStyle/>
-        {children}
-      </BaseProvider>
-    </StyletronProvider>
-  </ThemeProvider>
-);
+const Theme = ({ theme, children }) => {
+  const engine = new Styletron();
+  const mergedTheme = createTheme({
+    ...LightTheme,
+    primaryFontFamily: theme.primaryFontFamily,
+  }, theme);
+  return (
+    <ThemeProvider theme={theme}>
+      <StyletronProvider value={engine}>
+        <BaseProvider theme={mergedTheme}>
+          <GlobalStyle/>
+          {children}
+        </BaseProvider>
+      </StyletronProvider>
+    </ThemeProvider>
+  );
+};
+
+Theme.propTypes = {
+  theme: PropTypes.object.isRequired,
+  children: PropTypes.any,
+};
+
+Theme.defaultProps = {
+  theme: defaultTheme,
+};
 
 export default Theme;
