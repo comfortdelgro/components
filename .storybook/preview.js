1b70b52b0f5ba27851861b6e770c24e12fde116e
Hein Htut Aung <zugoripls@gmail.com>
2022-07-08 00:51:59 +0630

    feat: optimize preview device list

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,10 @@
 import { addDecorator } from "@storybook/react";
 import { withContexts } from "@storybook/addon-contexts/react";
-import { INITIAL_VIEWPORTS, DEFAULT_VIEWPORT, MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { INITIAL_VIEWPORTS, MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { contexts } from "./contexts";
 export const parameters = {
   viewport: {
-    viewports: { ...INITIAL_VIEWPORTS, ...MINIMAL_VIEWPORTS },
-    defaultViewport: 'tablet',
+    viewports: {  ...MINIMAL_VIEWPORTS },
   },
 };
 
