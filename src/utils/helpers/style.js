dacb7c195c8c52fe0bf0252e8a37610dd176b1f9
Hein Htut Aung <zugoripls@gmail.com>
2022-07-08 00:49:31 +0630

    feat: noPadding func+refactor

--- src/utils/helpers/style.js
+++ src/utils/helpers/style.js
@@ -1,3 +1,12 @@
+/**
+ * Apply given params to all (4) sides
+ * of an element.
+ * 
+ * @param {string} prefix
+ * @param {string} postfix 
+ * @param {array|string} value
+ * @returns {object}
+ */
 export function applyToAllSides(prefix, postfix, value) {
   const values = (typeof value === 'string')
                     ? Array(4).fill(value)
@@ -10,6 +19,14 @@ export function applyToAllSides(prefix, postfix, value) {
   };
 }
 
+/**
+ * Apply given params to all (4) corners
+ * of an element.
+ * @param {string} prefix
+ * @param {string} postfix 
+ * @param {array|string} value
+ * @returns {object}
+ */
 export function applyToAllCorners(prefix, postfix,value) {
   const values = (typeof value === 'string')
                     ? Array(4).fill(value)
@@ -22,50 +39,39 @@ export function applyToAllCorners(prefix, postfix,value) {
   };
 }
 
-// export function styleAllSides(name, value) {
-//   const values = (typeof value === 'string')
-//                     ? Array(4).fill(value)
-//                     : value;
-//   return {
-//     [`${name}TopStyle`]: values[0],
-//     [`${name}RightStyle`]: values[1],
-//     [`${name}BottomStyle`]: values[2],
-//     [`${name}LeftStyle`]: values[3],
-//   };
-// }
-
-// export function widthAllSides(name, value) {
-//   const values = (typeof value === 'string')
-//                     ? Array(4).fill(value)
-//                     : value;
-//   return {
-//     [`${name}TopWidth`]: values[0],
-//     [`${name}RightWidth`]: values[1],
-//     [`${name}BottomWidth`]: values[2],
-//     [`${name}LeftWidth`]: values[3],
-//   };
-// }
-
-// export function colorAllSides(name, value) {
-//   const values = (typeof value === 'string')
-//                     ? Array(4).fill(value)
-//                     : value;
-//   return {
-//     [`${name}TopColor`]: values[0],
-//     [`${name}RightColor`]: values[1],
-//     [`${name}BottomColor`]: values[2],
-//     [`${name}LeftColor`]: values[3],
-//   };
-// }
-
+/**
+ * Expand given margin style string
+ * to array.
+ * 
+ * @param {string} value
+ * @returns {object}
+ */
 export function margin(value) {
   return applyToAllSides("margin", "",value.split(" "));
 }
 
+/**
+ * Expand given padding style string
+ * to array and apply it to all sides.
+ * 
+ * @param {string} value 
+ * @returns {object}
+ */
 export function padding(value) {
-  return applyToAllSides("padding", "",value.split(" "));
+  const values = value.split(" ");
+  if (values.length < 4) {
+    throw new Error(`Shorthened padding style is not allowed. padding: ${values}`);
+  }
+  return applyToAllSides("padding", "", values);
 }
 
+/**
+ * Expand given border style string
+ * to array and apply it to all sides.
+ * 
+ * @param {string} value 
+ * @returns {object}
+ */
 export function border(value) {
   const prefix = "border";
   const values = value.split(" ");
@@ -76,6 +82,14 @@ export function border(value) {
   }
 }
 
+/**
+ * Expand given border style string 
+ * (withour border-color value)
+ * to array  and apply it to all sides.
+ * 
+ * @param {string} value 
+ * @returns {object}
+ */
 export function borderWithoutColors(value) {
   const prefix = "border";
   const values = value.split(" ");
@@ -85,18 +99,46 @@ export function borderWithoutColors(value) {
   }
 }
 
+/**
+ * Expand given border color style string
+ * to array and apply it to all sides.
+ * 
+ * @param {string} value 
+ * @returns {object}
+ */
 export function borderColors(value) {
   return applyToAllSides("border", "Color", value);
 }
 
+/**
+ * Expend given border-raduis value to
+ * array which apply it to all corners.
+ * 
+ * @param {string} value 
+ * @returns {object}
+ */
 export function borderRadius(value) {
   return applyToAllCorners("border", "Radius", value);
 }
 
+/**
+ * Unset border to all four sides
+ * 
+ * @returns {object}
+ */
 export function noBorder() {
   const prefix = "border";
   return {
     ...applyToAllSides(prefix, "width",0),
     ...applyToAllSides(prefix, "style", "none"),
   }
+}
+
+/**
+ * Unset padding to all four sides
+ * 
+ * @returns {object}
+ */
+export function noPadding() {
+  return applyToAllSides("padding", "", "unset");
 }
